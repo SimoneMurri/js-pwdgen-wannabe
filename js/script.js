66e5a16d72055b323d7b69c2656b50f69de03a17
Simone Murri <simonemurri701@gmail.com>
2023-11-09 15:47:18 +0100

    js primo esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let name = prompt('type a name');
+let surname = prompt('type a surname');
+let favoriteColor = prompt('favoriteColor');
+
+let sum = 0
+
+sum = name + surname + favoriteColor;
+
+document.getElementById('number').innerHTML = sum + 21
